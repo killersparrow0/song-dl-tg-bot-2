1726600c15f7fbcc8eab86092f4082c1f7097d7f
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-10-02 09:42:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,8 +86,7 @@ async def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "🎵 uploaded by @Misasongdl_bot
-               🎸 join @movies_songs_tj"
+        rep = "🎵 uploaded by @Misasongdl_bot"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
