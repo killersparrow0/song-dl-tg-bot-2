a3d604dce8f339e7ef862b180a9a1c49b3e63912
Milas <84676044+killersparrow0@users.noreply.github.com>
2021-10-03 16:02:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# Infinity BOTs <https://t.me/Infinity_BOTs>
+# ©
 
 import os
-API_ID = int(os.getenv("API_ID", "4080786"))
-API_HASH = os.getenv("API_HASH", "b214aec132378022e02df812714ba7a4")
-BOT_TOKEN = os.getenv("1921576203:AAHe8_acG372z6rszUx_CC7PMKDxpTmKGxY")
+    BOT_TOKEN = "1921576203:AAHe8_acG372z6rszUx_CC7PMKDxpTmKGxY" # 🌚 get it from @botfather (telegram) by sending /newbot command.
+    API_ID = "4080786" # 🌚 Same as APP_ID. Get it from my.telegram.org.
+    API_HASH = "b214aec132378022e02df812714ba7a4" # 🌚 Get it from my.telegram.org.
